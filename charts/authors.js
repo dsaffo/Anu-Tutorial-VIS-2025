70aefc824abf73b80f8919f129d09242d363e2ac
benjaminchlee <benjaminchlee@gmail.com>
2025-10-30 20:46:50 -0400

    Update network

--- charts/authors.js
+++ charts/authors.js
@@ -1,18 +1,18 @@
 import * as anu from '@jpmorganchase/anu';
 import * as BABYLON from '@babylonjs/core';
+import * as GUI from '@babylonjs/gui';
 import * as d3 from 'd3';
-import * as d3force from 'd3-force-3d'; 
+import * as d3force from 'd3-force-3d';
 import authorsGraph from '../data/authorGraph.json' assert {type: 'json'};  //Our data
 
-export const authorsNetwork  = async (scene) => { 
+export const authorsNetwork = async (scene) => {
 
-     //Create a D3 color scale using Anu helper functions to map values to Color4 objects with colors based on the 'schemecategory10' palette from D3
-  let scaleC = d3.scaleOrdinal(anu.ordinalChromatic('d310').toColor4());
-
-  //Create a D3 simulation with several forces, this function mutates the data object we pass in so we make a deep clone of it first
+  //Create a deep clone of our data, as the simulation  mutates it
   let data = JSON.parse(JSON.stringify(authorsGraph));
+
+  //Create our D3 simulation
   let simulation = d3force.forceSimulation(data.nodes, 3)
-                          .force('link', d3force.forceLink(data.links))
+                          .force('link', d3force.forceLink(data.links).id(d => d.id))
                           .force('charge', d3force.forceManyBody())
                           .force('collide', d3force.forceCollide())
                           .force('center', d3force.forceCenter(0, 0, 0))
@@ -20,20 +20,20 @@ export const authorsNetwork  = async (scene) => {
                           .on('end', () => simulation.stop());
 
   //We use Mesh instancing here for better performance, first we create a Mesh that serves as the root Node
-  let rootSphere = anu.create('sphere', 'node', { diameter: 5 });
+  let rootSphere = anu.create('sphere', 'node', { diameter: 10, segments: 8 });
   rootSphere.isVisible = false;
-  rootSphere.registerInstancedBuffer('color', 4);   //We need an InstancedBuffer to set the color of instances
-  
-  //Create a Center of Transform TransformNode that serves the parent node for all our meshes that make up our chart
-  let CoT = anu.create('cot', 'cot');
-  //Select our CoT so that we have it as a Selection object
-  let network = anu.selectName('cot', scene);
+  rootSphere.registerInstancedBuffer('color', 4);
 
-  //Create instanced sphere meshes from our rootSphere as children of our CoT for each row of our data and set their initial visual encodings using method chaining
+  //Create a Center of Transform that will be the parent node of our network
+  let network = anu.bind('cot', undefined, [undefined]);
+
+  //Create a D3 color scale of the 'schemecategory10' palette to map data to Color4 objects
+  let scaleC = d3.scaleOrdinal(anu.ordinalChromatic('d310').toColor4());
+
+  //Create our nodes, we will set their position in the simulation
   let nodes = network.bindInstance(rootSphere, data.nodes)
-                       .position((d) => new BABYLON.Vector3(d.x, d.y, d.z))
-                       .setInstancedBuffer('color', (d) => scaleC(d.group))
-                       .id((d) => d.id);
+                      .setInstancedBuffer('color', (d) => scaleC(d.affiliation))
+                      .id((d) => d.id);
 
   //Create a helper function that will return us an array of arrays where each sub-array is the start and end Vector3 of each link
   function dataToLinks(data) {
@@ -47,19 +47,18 @@ export const authorsNetwork  = async (scene) => {
   }
 
   //Create our lineSystem mesh using our data and helper function from above
-  let links = network.bind('lineSystem', { lines: (d) => dataToLinks(d), updatable: true }, [data.links])
-                     .prop('color', new BABYLON.Color4(1, 1, 1, 1));
+  let links = network.bind('lineSystem', { lines: (d) => dataToLinks(d), updatable: true }, [data.links]);
 
-  //Update the position of the nodes and links each time the simulation ticks
+  //Create the callback that will run every simulation tick to update our node and links
   function ticked() {
-    //For the instanced spheres just set a new position
+    //For the instanced spheres, just set a new position based on values populated by the simulation
     nodes.position((d) => (new BABYLON.Vector3(d.x, d.y, d.z)));
-    //For the links use the run method to replace the lineSystem mesh with a new one, passing in the mesh into the instance option
-    links.run((d, n, i) => anu.create('lineSystem', 'edge', { lines: dataToLinks(d), instance: n, updatable: true }, d));
+    //For the links, use the run method to replace the lineSystem mesh with a new one, passing in the mesh into the instance option
+    links.run((d,n,i) => anu.create('lineSystem', 'edge', { lines: dataToLinks(d), instance: n, updatable: true }, d));
   }
 
   //The network can get quite big in size (spatial size), so here we run a function to scale the entire network down to a 1x1x1 box
   network.run((d,n,i) => n.normalizeToUnitCube());
 
-  return scene
+  return scene;
 }
